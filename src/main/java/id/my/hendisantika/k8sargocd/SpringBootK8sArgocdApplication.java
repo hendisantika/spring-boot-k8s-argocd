0a44f831c2a2fd69b24d57e63d0474f46cb647bf
hendisantika <hendisantika@yahoo.co.id>
2025-05-11 05:44:17 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/k8sargocd/SpringBootK8sArgocdApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.k8sargocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sArgocdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootK8sArgocdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/k8sargocd/SpringBootK8sArgocdApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.k8sargocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sArgocdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
