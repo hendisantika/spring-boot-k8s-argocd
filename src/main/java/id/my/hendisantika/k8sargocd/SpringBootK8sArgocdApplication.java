65c154a8928aaf77db547494e4ecd27d3d648c54
hendisantika <hendisantika@yahoo.co.id>
2025-05-11 05:45:46 +0700

    feat: Add ApplicationRunner🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/k8sargocd/SpringBootK8sArgocdApplication.java
+++ src/main/java/id/my/hendisantika/k8sargocd/SpringBootK8sArgocdApplication.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.k8sargocd;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootK8sArgocdApplication {
 
@@ -10,4 +14,10 @@ public class SpringBootK8sArgocdApplication {
         SpringApplication.run(SpringBootK8sArgocdApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner runner() {
+        return args -> {
+            log.info("Application Version: V2");
+        };
+    }
 }
